71445a4a0afd8a6ef426f6604a1608662a5ec920
tk-sylc <hasunuma@ms.sylc.co.jp>
2026-09-02 11:28:22 +0900

    feat: add asset request portal

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react'
+import { sites } from '@openai/sites-vite-plugin'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sites()],
 })
